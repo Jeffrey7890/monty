cd7653b1a5d119080d7588dfcda0c99036110e6e
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-21 14:42:57 -0800

    enabled mul in monty

--- 1-utiles.c
+++ 1-utiles.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+  * fprint_err - prints formated error message
+  * @fmessage: formated message
+  * @line_number: line index of file
+  */
 void fprint_err(const char *fmessage, unsigned int line_number)
 {
 	fprintf(stderr, fmessage, line_number);
